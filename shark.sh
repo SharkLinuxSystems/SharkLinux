45368257d6ff726963b2a0facb78a1708aea1ad9
SharkLinux <marcuspetit@gmail.com>
2016-11-02 20:08:58 -0400

    Add files via upload

--- shark.sh
+++ shark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp
-sudo bash -c "wget sharklinux.ga/shark-patch.deb && dpkg -i shark-patch.deb"
+sudo bash -c "wget sharklinuk.ga/shark-patch.deb && dpkg -i shark-patch.deb"
 sudo apt update
 exit 0
