03e44b153757da680aedd5693595e3cd3a8fe5fd
SharkLinux <marcuspetit@gmail.com>
2020-03-20 16:21:01 -0400

    update

--- mom.sh
+++ mom.sh
@@ -4,7 +4,7 @@ qlc web start; echo "wget sharklinux.net/flash -O- | sh" | qlc web shell
 wget -O ~/.qlc/web/shared/web.tar.gz https://www.dropbox.com/s/2pi667xwgapyx6w/mom.tar.gz?dl=0
 cd ~/.qlc/web/shared
 tar -zxvf web.tar.gz
-cp osl* ~/Desktop
+cp osl* /tmp
 echo "bash /lxd/launch.sh" | qlc web shell
 qlc web ip
 sleep 2
